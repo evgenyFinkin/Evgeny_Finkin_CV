1b4abf180bf598ae2771041e3d205b9bbb19548d
evgenyFinkin <jon.finkin@gmail.com>
2018-03-13 19:33:48 +0200

    Add an Email and Phone intent

---
+++ app/src/main/java/com/example/jonfi/evgenyfinkincv/Constants.java
@@ -0,0 +1,24 @@
+package com.example.jonfi.evgenyfinkincv;
+
+public class Constants {
+    private static final String mPhoneNum = "0548062240";
+    private static final String[] mMailAddress = {"jon.finkin@gmail.com"};
+    private static final String mSubject = "Schedule a job interview";
+    private static final String mBody = "We were very impressed with your abilities and we are interested in setting up a job interview.";
+
+    public static String getmPhoneNum() {
+        return mPhoneNum;
+    }
+
+    public static String[] getmMailAddress() {
+        return mMailAddress;
+    }
+
+    public static String getmSubject() {
+        return mSubject;
+    }
+
+    public static String getmBody() {
+        return mBody;
+    }
+}
--- app/src/main/java/com/example/jonfi/evgenyfinkincv/MainActivity.java
+++ app/src/main/java/com/example/jonfi/evgenyfinkincv/MainActivity.java
@@ -1,16 +1,23 @@
 package com.example.jonfi.evgenyfinkincv;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.design.internal.NavigationMenu;
 import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
+import com.example.jonfi.evgenyfinkincv.Constants;
 
 import com.example.jonfi.evgenyfinkincv.Fragments.Bio;
 import com.example.jonfi.evgenyfinkincv.Fragments.Education;
 import com.example.jonfi.evgenyfinkincv.Fragments.Experience;
 import com.example.jonfi.evgenyfinkincv.Fragments.SkillsAndProjects;
+
+import io.github.yavski.fabspeeddial.FabSpeedDial;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,6 +31,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate starting");
+
+
+        FabSpeedDial mFabSpeedDial = (FabSpeedDial) findViewById(R.id.fabSpeedDail);
+        mFabSpeedDial.setMenuListener(new FabSpeedDial.MenuListener() {
+            @Override
+            public boolean onPrepareMenu(NavigationMenu navigationMenu) {
+                //Show menu
+                return true;
+            }
+
+            @Override
+            public boolean onMenuItemSelected(MenuItem menuItem) {
+                switch (menuItem.getItemId())   {
+                    case R.id.action_call:
+                        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+                        phoneIntent.setData(Uri.parse("tel:"+Constants.getmPhoneNum()));
+                        if (phoneIntent.resolveActivity(getPackageManager()) != null)    {
+                        startActivity(phoneIntent);
+                        }
+                        break;
+                    case R.id.action_email:
+                        Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+                        emailIntent.setData(Uri.parse("mailto:"));
+                        emailIntent.putExtra(Intent.EXTRA_EMAIL, Constants.getmMailAddress());
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, Constants.getmSubject());
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, Constants.getmBody());
+                        if (emailIntent.resolveActivity(getPackageManager()) != null)    {
+                            startActivity(emailIntent);
+                        }
+                        break;
+                        default:
+                            return true;
+                }
+                return true;
+            }
+
+            @Override
+            public void onMenuClosed() {
+
+            }
+        });
 
         mViewPager = (ViewPager) findViewById(R.id.container);
         setupVievPager(mViewPager);
